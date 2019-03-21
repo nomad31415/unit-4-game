cc3017ce814d9805d4db5788cf31ca386feacebd
mcatoeff@gmail.com <mcatoeff@gmail.com>
2019-03-20 17:13:21 -0700

    overWrite by normal code/tested -> no rush!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,107 +1,95 @@
-//JS Code
+'use strict';
 
-$(document).ready(function () {
-    // Generates random number to guess
-    var Random = Math.floor(Math.random() * 120 + 19);
+// Global variabls -> not many.
 
-    // Display random number
-    $('#scoreToMatch').text(Random);
+let crystals = [0, 0, 0, 0];
+let crystalClicked = 0;
+let reducedArray = 0;
 
-    //Random number for each crystal
-    var red = Math.floor(Math.random() * 12 + 1);
-    var blue = Math.floor(Math.random() * 12 + 1);
-    var yellow = Math.floor(Math.random() * 12 + 1);
-    var green = Math.floor(Math.random() * 12 + 1);
-
-    // Start Varibles wins, losses and total
-    var playerUp = 0;
-    var wins = 0;
-    var losses = 0;
-
-    $('#wins').text(wins);
-    $('#losses').text(losses);
-
-    // Reset game
-    function reset() {
-        Random = Math.floor(Math.random() * 120 + 19);
-        console.log(Random)
-        $('#scoreToMatch').text(Random);
-        red = Math.floor(Math.random() * 12 + 1);
-        blue = Math.floor(Math.random() * 12 + 1);
-        yellow = Math.floor(Math.random() * 12 + 1);
-        green = Math.floor(Math.random() * 12 + 1);
-        playerUp = 0;
-        $('#totalScore').text(playerUp);
-    }
-    // Display wins
-    function win() {
-        alert("Congrats! You won!");
-        wins++;
-        $('#wins').text(wins);
-        reset();
-    }
-
-    // Display losses
-    function loss() {
-        alert("Sorry! You lose!");
-        losses++;
-        $('#losses').text(losses);
-        reset()
-    }
-    // Clicking crystals
-
-    $(".imgthumbnailred").on("click", function () {
-        playerUp = playerUp + red;
-        console.log("New Player Total= " + playerUp);
-        $('#totalScore').text(playerUp);
-
-        if (playerUp == Random) {
-            win();
-        }
-
-        else if (playerUp > Random) {
-            loss();
-        }
-    });
-    $(".imgthumbnailblue").on("click", function () {
-        playerUp = playerUp + blue;
-        console.log("New Player Total= " + playerUp);
-        $('#totalScore').text(playerUp);
-
-        if (playerUp == Random) {
-            win();
-        }
-
-        else if (playerUp > Random) {
-            loss();
-        }
-    });
-    $(".imgthumbnailyellow").on("click", function () {
-        playerUp = playerUp + yellow;
-        console.log("New Player Total= " + playerUp);
-        $('#totalScore').text(playerUp);
-
-        if (playerUp == Random) {
-            win();
-        }
-
-        else if (playerUp > Random) {
-            loss();
-        }
-    });
-    $(".imgthumbnailgreen").on("click", function () {
-        playerUp = playerUp + green;
-        console.log("New Player Total= " + playerUp);
-        $('#totalScore').text(playerUp);
-
-        if (playerUp == Random) {
-            win();
-        }
-
-        else if (playerUp > Random) {
-            loss();
-        }
-    });
+let wins = 0;
+let losses = 0;
+let targetNum = 0;
+let userNum = [0];
 
 
+// When page loads start a new game
+$(document).ready(function() {
+    startNewGame();
 });
+
+// Generate new random numbers for targetNum and Crystals, append targetNum to DOM and start the game
+const startNewGame = function() {
+    genRandomNum();
+    resetDOM();
+    gameLevel();
+};
+
+// Generate random number between 19-120 for the targetNum and between 1-12 for all for crystals
+const genRandomNum = function() {
+    targetNum = Math.floor(Math.random() * 101) + 19;
+    for (let i = 0; i < 4; i++) {
+        crystals[i] = Math.floor(Math.random() * 12) + 1;
+    }
+};
+
+// Append wins, losses, targetNum and the current userNum to the DOM
+const resetDOM = function() {
+    $('.wins').text(wins);
+    $('.losses').text(losses);
+    userNum[0] = 0;
+    $('.users-num').text(userNum[0]);
+    $('.target-num').text(targetNum);
+};
+
+// The Game over display to DOM on lossing game termination
+const gameOver = function() {
+    $(".wins").empty();
+    $(".losses").text("Game Over");
+};
+
+// You win display to DOM on winning game termination
+const finalWin = function() {
+    $(".losses").empty();
+    $(".wins").text("You Won!");
+};
+
+// Listenig for event when a crystal image is clicked
+const gameLevel = function() {
+    $('.crystal').click(function() {
+        // Grab the value attribute of the specific crystal imaged clicked and parse it to an integer, 
+        // set it to varible crystalClicked
+        crystalClicked = parseInt($(this).attr("value"));
+        // Push that specific crystal's random number to the userNum array. 
+        // Note: crystalClick minus 1 is to account for zero index of crystals array. 
+        userNum.push(crystals[crystalClicked-1]);
+        // Reduce userNum array to reducedArray variable
+        reducedArray = userNum.reduce(function(accu, elem) {
+            return accu + elem;
+        }, 0);
+        // Clear userNum array and reset zero index to reducedArray number. Update userNum in the DOM.
+        userNum = [];
+        userNum[0] = reducedArray;
+        $('.users-num').text(userNum[0]);
+        // Check for a win or loss. Max wins and losses before game terminates is 9 times, respectively.
+        if (userNum[0] === targetNum) {
+            wins++;
+            if (wins > 9) {
+                $('.crystal').off('click');
+                finalWin();
+                return;
+            };
+            $('.crystal').off('click');
+            startNewGame();
+        } else if (userNum[0] > targetNum) {
+            losses++;
+            if (losses > 9) {
+                $('.crystal').off('click');
+                gameOver();
+                return;
+            };
+            $('.crystal').off('click');
+            startNewGame();
+        }
+    });
+};
+
